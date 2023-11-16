23d42991bffa80a8144ca72159900336fcee5e4c
unknown <2637833054@qq.com>
2023-11-16 15:44:06 +0800

    新增source map的打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // build: {
+  //   sourcemap: true
+  // },
   base: '/bim_CAD/',
   // base: '/',
   plugins: [
